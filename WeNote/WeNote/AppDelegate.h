933d8c15e40ceab468424b8b4767f4901e04e909
WangPan <wangpantech@outlook.com>
2016-03-28 10:16:10 +0800

    Create PJ

---
+++ WeNote/WeNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeNote
+//
+//  Created by WangPan on 16/3/28.
+//  Copyright © 2016年 Wang Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
