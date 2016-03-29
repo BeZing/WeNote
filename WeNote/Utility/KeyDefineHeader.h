43a832ea41a0e61b4efb051060fdcdfdd336077b
WangPan <wangpantech@outlook.com>
2016-03-29 15:11:10 +0800

    hptextfix

---
+++ WeNote/Utility/KeyDefineHeader.h
@@ -0,0 +1,13 @@
+//
+//  KeyDefineHeader.h
+//  WeNote
+//
+//  Created by WangPan on 16/3/29.
+//  Copyright © 2016年 Wang Pan. All rights reserved.
+//
+
+#ifndef KeyDefineHeader_h
+#define KeyDefineHeader_h
+#define CUSFONTKEY @"customFontKey"
+
+#endif /* KeyDefineHeader_h */
